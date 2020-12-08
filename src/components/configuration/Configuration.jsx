94ad80538ef44ac3695cd8775fff7bea0e195760
Tim <tim.langner@tobit.com>
2020-12-08 16:13:42 +0100

    :construction: Add configuration tooltip

---
+++ src/components/configuration/Configuration.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import './configuration.scss';
+import {Checkbox, Input} from "chayns-components";
+
+const Configuration = ({currentHeight}) => {
+
+    const [mobileHeight, setMobileHeight] = useState(currentHeight / 3);
+
+    return (
+        <div className="container">
+            <div className="input-wrapper">
+                <p>Desktop</p>
+                <Input
+                    className="input--desktop"
+                    design={1}
+                    dynamic={1}
+                    placeholder="Pixel"
+                    value={currentHeight}
+                />
+            </div>
+            <div style={{ marginTop: '5px' }} />
+            <div className="input-wrapper">
+                <p>Mobil</p>
+                <Input
+                    className="input--desktop"
+                    design={1}
+                    dynamic={1}
+                    placeholder="Pixel"
+                    defaultValue="15"
+                    disabled
+                />
+            </div>
+            <div style={{ marginTop: '10px' }} />
+            <Checkbox label="Als Standard festlegen" />
+        </div>
+    );
+};
+
+export default Configuration;
--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,13 +1,40 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import ResizePanel from "react-resize-panel";
 import './container.scss';
+import {Button, Tooltip} from "chayns-components";
+import Configuration from "../configuration/Configuration";
 
 const Container = () => {
+    const tooltip = useRef(null)
+
+    const [showTooltip, setShowTooltip] = useState(false);
+
+    const handleTooltip = () => {
+        if (!showTooltip) {
+            tooltip.show();
+        } else {
+            tooltip.hide();
+        }
+        setShowTooltip(!tooltip);
+    }
+
     return (
         <>
-            <ResizePanel direction='s' handleClass="customHandle" style={{border: '2px solid darkgrey', backgroundColor: 'lightgrey', height: '100px'}}>
-                <div style={{height: '100%'}} />
+            <div style={{ textAlign: 'right', marginBottom: '20px' }}>
+                <Button
+                    onClick={() => {
+                        handleTooltip();
+                    }}
+                >
+                    Open Tooltip
+                </Button>
+            </div>
+            <ResizePanel direction='s' handleClass="customHandle" style={{border: '2px solid darkgrey', backgroundColor: 'lightgrey', height: '150px'}}>
+                <div style={{height: '100%'}}>
+                </div>
             </ResizePanel>
+            <br />
+            <Configuration currentHeight={60}/>
         </>
     );
 };
