2687ee9cb1a962a721756fe4080bfa955828b8cb
Tim <tim.langner@tobit.com>
2020-12-08 17:07:01 +0100

    :art: Dynamically change mobile value

--- src/components/configuration/Configuration.jsx
+++ src/components/configuration/Configuration.jsx
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from "react";
 import './configuration.scss';
 import {Checkbox, Input} from "chayns-components";
 
-const Configuration = ({currentHeight}) => {
+const Configuration = ({currentHeight, setContainerHeight}) => {
 
-    const [mobileHeight, setMobileHeight] = useState();
+    const [desktopHeight, setDesktopHeight] = useState(0)
+    const [mobileHeight, setMobileHeight] = useState(0);
 
     useEffect(() => {
+        setDesktopHeight(currentHeight);
         setMobileHeight((currentHeight / 3).toFixed(0));
     }, [currentHeight]);
+
+    useEffect(() => {
+        setMobileHeight((desktopHeight / 3).toFixed(0));
+    }, [desktopHeight]);
 
     return (
         <div className="container">
@@ -19,7 +25,12 @@ const Configuration = ({currentHeight}) => {
                     design={1}
                     dynamic={1}
                     placeholder="Pixel"
-                    value={currentHeight}
+                    value={desktopHeight}
+                    onChange={(value) => {
+                        console.log(value);
+                        setDesktopHeight(value);
+                        setContainerHeight(value);
+                    }}
                 />
             </div>
             <div style={{ marginTop: '5px' }} />
