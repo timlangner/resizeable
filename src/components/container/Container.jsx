60b3d9248292306ddc456bf177424617fd2f6b79
Tim <tim.langner@tobit.com>
2020-12-08 17:21:58 +0100

    :art: Preparation for manual container resize

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ResizePanel from "react-resize-panel";
 import './container.scss';
 import useResizeObserver from "use-resize-observer";
@@ -6,14 +6,15 @@ import Configuration from "../configuration/Configuration";
 
 const Container = () => {
     const { ref, height } = useResizeObserver();
+    const [containerHeight, setContainerHeight] = useState(150);
 
     return (
         <>
-            <ResizePanel direction='s' handleClass="customHandle" style={{border: '2px solid darkgrey', backgroundColor: 'lightgrey', height: '150px'}}>
+            <ResizePanel direction='s' handleClass="customHandle" style={{border: '2px solid darkgrey', backgroundColor: 'lightgrey', height: containerHeight}}>
                 <div ref={ref} style={{height: '100%'}} />
             </ResizePanel>
             <br />
-            <Configuration currentHeight={height} />
+            <Configuration currentHeight={height} setContainerHeight={setContainerHeight} />
         </>
     );
 };
