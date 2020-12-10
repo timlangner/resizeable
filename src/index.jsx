367793dbcae66fa656abf682ff1e5fd455f9ad7f
Tim <tim.langner@tobit.com>
2020-12-10 15:18:34 +0100

    :sparkles: Disallow refresh scroll for mobile

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,8 @@ import App from './components/App';
 async function init() {
     try {
         await chayns.ready;
+        await chayns.disallowRefreshScroll();
+        await chayns.invokeCall({ action: 0, value: { enabled: false } });
 
         ReactDOM.render(
             <App />,
