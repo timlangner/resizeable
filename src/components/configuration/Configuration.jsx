ba028e6812d022e333ebe2ef6b7c3bf5116e5fba
Tim <tim.langner@tobit.com>
2020-12-10 15:18:58 +0100

    :lipstick: Add lines who point to the values

--- src/components/configuration/Configuration.jsx
+++ src/components/configuration/Configuration.jsx
@@ -1,56 +1,87 @@
 import React, { useState, useEffect } from "react";
 import {Checkbox, Input} from "chayns-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock, faUnlock } from "@fortawesome/free-solid-svg-icons";
 import './configuration.scss';
 
-const Configuration = ({currentHeight, setContainerHeight}) => {
+const Configuration = ({currentHeight}) => {
 
     const [desktopHeight, setDesktopHeight] = useState(0)
     const [mobileHeight, setMobileHeight] = useState(0);
-    const [isFixed, setIsFixed] = useState(false);
+    const [isLocked, setIsLocked] = useState(true);
 
     useEffect(() => {
-        setDesktopHeight(currentHeight);
-        if (!isFixed) {
-            setMobileHeight((currentHeight / 3).toFixed(0));
-        }
+        chayns.env.isDesktop ? setDesktopHeight(currentHeight) : setMobileHeight(currentHeight);
     }, [currentHeight]);
 
     useEffect(() => {
-        if (!isFixed) {
+        if (isLocked) {
             setMobileHeight((desktopHeight / 3).toFixed(0));
         }
     }, [desktopHeight]);
 
+    useEffect(() => {
+        if (isLocked) {
+            setDesktopHeight((mobileHeight * 3).toFixed(0));
+        }
+    }, [mobileHeight]);
+
     return (
         <div className="container">
-            <div className="input-wrapper">
-                <p>Desktop</p>
-                <Input
-                    className="input--desktop"
-                    design={1}
-                    dynamic={1}
-                    placeholder="Pixel"
-                    value={desktopHeight}
-                    onChange={(value) => {
-                        setDesktopHeight(value);
-                        setContainerHeight(value);
-                    }}
-                />
-            </div>
-            <div style={{ marginTop: '5px' }} />
-            <div className="input-wrapper">
-                <p>Mobil</p>
-                <Input
-                    className={isFixed ? 'input--desktop' : 'input--desktop input--mobile'}
-                    design={1}
-                    dynamic={1}
-                    placeholder="Pixel"
-                    value={mobileHeight}
-                    onChange={(value) => {
-                        setMobileHeight(value);
-                        setIsFixed(true);
-                    }}
-                />
+            <div className="wrapper">
+                <div>
+                    <div className="input-wrapper">
+                        <p>Desktop</p>
+                        <Input
+                            className="input--box"
+                            type="number"
+                            design={1}
+                            dynamic={1}
+                            placeholder="Pixel"
+                            value={desktopHeight}
+                            onChange={(value) => {
+                                setDesktopHeight(parseInt(value));
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <div style={{ marginTop: '5px' }} />
+                    </div>
+                    <div className="input-wrapper">
+                        <p>Mobil</p>
+                        <Input
+                            className="input--box"
+                            type="number"
+                            design={1}
+                            dynamic={1}
+                            placeholder="Pixel"
+                            value={mobileHeight}
+                            onChange={(value) => {
+                                setMobileHeight(parseInt(value));
+                            }}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div className="upper-link--left" />
+                        <div className="upper-link--bottom" />
+                    </div>
+                    <div
+                        className="icon"
+                        style={isLocked ? { color: '#696969' } : { color: 'darkgrey' }}
+                        onClick={() => {
+                            console.log('onClick', typeof desktopHeight);
+                            setIsLocked(!isLocked);
+                        }}
+                    >
+                        <FontAwesomeIcon icon={isLocked ? faLock : faUnlock} />
+                    </div>
+                    <div>
+                        <div className="lower-link--top" />
+                        <div className="lower-link--left" />
+                    </div>
+                </div>
             </div>
             <div style={{ marginTop: '10px' }} />
             <Checkbox label="Als Standard festlegen" />
