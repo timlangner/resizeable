40d83606693775f00c1991f6dbe16f1463d39991
Tim <tim.langner@tobit.com>
2020-12-11 10:35:35 +0100

    :sparkles: Always make tapp the same height as the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Container from "./container/Container";
 
 import './app.scss';
 
 const App = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        chayns.getWindowMetrics().then((windowData) => {
+            setData(windowData);
+        });
+    }, []);
+
     return (
-        <>
+        <div
+            style={data ? { height: data.windowHeight - data.offsetTop - data.frameY } : { height: '100%' }}
+        >
             <Container />
-        </>
+        </div>
     );
 }
 
