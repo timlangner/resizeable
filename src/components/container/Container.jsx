5d63d9b9581784e022215213c127291c5c3ee50a
Tim <tim.langner@tobit.com>
2020-12-11 09:34:33 +0100

    :lipstick: Add pagemaker toolbar

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -2,17 +2,25 @@ import React, { useState, useEffect } from "react";
 import ResizePanel from "react-resize-panel";
 import useResizeObserver from "use-resize-observer";
 import Configuration from "../configuration/Configuration";
+import Toolbar from "../pagemaker/toolbar/Toolbar";
 import './container.scss';
 
 const Container = () => {
     const { ref, height } = useResizeObserver();
     const [containerHeight, setContainerHeight] = useState(150);
+    const [showToolbar, setShowToolbar] = useState(false);
 
     return (
         <>
-            <ResizePanel direction='s' handleClass="customHandle" style={{ height: containerHeight, border: '2px solid darkgrey', backgroundColor: 'lightgrey' }}>
-                <div ref={ref} style={{height: '100%'}} />
-            </ResizePanel>
+            <div
+                onMouseEnter={() => setShowToolbar(true)}
+                onMouseLeave={() => setShowToolbar(false)}
+            >
+                <Toolbar style={showToolbar ? { display: 'none' } : { display: 'none' }} />
+                <ResizePanel direction='s' handleClass="customHandle" style={{ height: containerHeight, border: '1px solid darkgrey' }}>
+                    <div ref={ref} style={{height: '100%'}} />
+                </ResizePanel>
+            </div>
             <br />
             <Configuration currentHeight={height} />
         </>
---
+++ src/components/pagemaker/toolbar/Toolbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Icon } from 'chayns-components/lib';
+import './toolbar.scss';
+
+const Toolbar =  () => (
+    <div className="toolbar--wrapper">
+        <div className="option--wrapper">
+            <div className="icon--wrapper">
+                <Icon icon="far fa-trash" />
+            </div>
+            <div className="icon--wrapper">
+                <Icon icon="far fa-arrows-v" />
+            </div>
+            <div className="icon--wrapper">
+                <Icon icon="far fa-clone" />
+            </div>
+            <div className="icon--wrapper">
+                <Icon icon="far fa-eye" />
+            </div>
+            <div className="icon--wrapper">
+                <Icon icon="far fa-align-justify" />
+            </div>
+        </div>
+    </div>
+);
+
+export default Toolbar;
