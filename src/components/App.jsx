d7fd0039ebe6a613d55030a8edcb1218982e321c
Tim <tim.langner@tobit.com>
2020-12-14 14:51:25 +0100

    :bug: Fix unscrollable page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,8 +13,17 @@ const App = () => {
 
     return (
         <div
-            style={data ? { height: data.windowHeight - data.offsetTop - data.frameY } : { height: '100%' }}
+            style={{ height: '100%' }}
         >
+            <h1>chayns® Design Guide</h1>
+            <p style={{ marginTop: '10px' }}>Dieser Design Guide ist das zentrale Element der Identität und prägt das visuelle Erscheinungsbild und die Usability von chayns®. Dies zeichnet sich durch den konsequenten Einsatz und das optimale Zusammenspiel der Basiselemente wie Farbe, Typografie und Layout aus. </p>
+            <div style={{ textAlign: 'center', marginBottom: '-30px' }}>
+                <img
+                    src="https://tsimg.cloud/75508-06235/d640573f2b25f04781cdc52c493a072458798fa9_fwebp-w650.png"
+                    alt="preview"
+                    style={{ width: '60%' }}
+                />
+            </div>
             <Container />
         </div>
     );
