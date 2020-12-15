cfc0fdad01e6a4b6f3bdc72fd41a37546f434da9
Tim <tim.langner@tobit.com>
2020-12-15 08:56:01 +0100

    :sparkles: Keep container active while resizing

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -7,33 +7,66 @@ import './container.scss';
 
 const Container = () => {
     const { ref, height } = useResizeObserver();
-    const [containerHeight] = useState(175);
+    const [containerHeight, setContainerHeight] = useState(175);
+    const [isActive, setIsActive] = useState(false);
+    const [isMouseDown, setIsMouseDown] = useState(false);
+
+    useEffect(() => {
+        if (height && height > 0) setContainerHeight(height + 2);
+    }, [height]);
+
+    useEffect(() => {
+        console.log(isActive);
+    }, [isActive]);
 
     return (
         <>
             <div style={{ height: '100%' }}>
-                <Toolbar />
-                <ResizePanel
-                    direction='s'
-                    handleClass="customHandle"
-                    style={{ height: containerHeight, border: '1px solid darkgrey' }}
+                {/*Fake Div*/}
+                <div
+                    style={!isActive ? { height: containerHeight + 40 } : null}
+                    onMouseOver={() => setIsActive(true)}
+                />
+                {/*TODO: Fix container loosing height after page load*/}
+                {/*Resizeable Div*/}
+                <div
+                    style={isActive ? { display: 'block' } : { display: 'none' }}
+                    onMouseDown={() => {
+                        setIsMouseDown(true);
+                        setIsActive(true);
+                    }}
+                    onMouseUp={() => setIsMouseDown(false)}
+                    onMouseLeave={() => {
+                        if (!isMouseDown) setIsActive(false);
+                    }}
                 >
-                    <div ref={ref} style={{height: '100%', display: 'flex', justifyContent: 'center'}} >
-                        <div style={height <= 120 ? { display: 'none' } : null}>
-                            <div style={{ marginTop: `${height - 145}px` }}>
-                                <Configuration currentHeight={height} position={"inside"} />
+                    <Toolbar />
+                    <ResizePanel
+                        direction='s'
+                        handleClass="customHandle"
+                        style={{ height: containerHeight, border: '1px solid darkgrey' }}
+                    >
+                        <div
+                            ref={ref}
+                            style={{height: '100%', display: 'flex', justifyContent: 'center', backgroundColor: '#e7e7e7'}}
+                        >
+                            <div style={height <= 120 ? { display: 'none' } : null}>
+                                <div style={{ marginTop: `${height - 145}px` }}>
+                                    <Configuration currentHeight={height} position={"inside"} />
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </ResizePanel>
-                <div className="config--wrapper" style={height > 120 ? { display: 'none' } : null}>
-                    <Configuration currentHeight={height} />
-                    <div style={{ position: 'absolute', width: '90%' }}>
-                        <h1 style={{ marginTop: '10px' }}>Schreibweisen</h1>
-                        <p>Zu einem ordentlichen Markenauftritt gehört nicht nur ein stimmiges Corporate Design, sondern auch ein einheitlicher Sprachgebrauch.</p>
+                    </ResizePanel>
+                    <div style={height > 120 ? { height: '10px'} : null} />
+                    <div className="config--wrapper" style={height > 120 ? { display: 'none' } : null}>
+                        <Configuration currentHeight={height} />
+                        <div style={chayns.env.isDesktop ? { position: 'absolute', width: '90%' } : { position: 'absolute', width: '96%', marginTop: '10px' }}>
+                            <h1 style={{ marginTop: '20px' }}>Schreibweisen</h1>
+                            <p>Zu einem ordentlichen Markenauftritt gehört nicht nur ein stimmiges Corporate Design, sondern auch ein einheitlicher Sprachgebrauch.</p>
+                        </div>
                     </div>
                 </div>
-                <div style={height <= 120 ? { display: 'none' } : null}>
+                <div style={!isActive ? null : height <= 120 ? { display: 'none' } : null}>
                     <h1 style={{ marginTop: '10px' }}>Schreibweisen</h1>
                     <p>Zu einem ordentlichen Markenauftritt gehört nicht nur ein stimmiges Corporate Design, sondern auch ein einheitlicher Sprachgebrauch.</p>
                 </div>
